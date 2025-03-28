9be7402b86494528013e0b71d3eecbcf58481fec
KarmaWSYD <powertuukka@gmail.com>
2025-03-28 18:06:57 +0200

    Adding test functions

--- app.py
+++ app.py
@@ -6,4 +6,25 @@ app = Flask(__name__)
 def hello():
     return "Hello, World! 2"
 
-# Koodi alle
+@app.route("/page1")
+def page1():
+    return "Tämä on sivu 1"
+
+@app.route("/page2")
+def page2():
+    return "Tämä on sivu 2"
+
+@app.route("/test")
+def test():
+    content = ""
+    for i in range(1, 101):
+        content += str(i) + " "
+    return content
+
+@app.route("/page/<int:page_id>")
+def page(page_id):
+    return "Tämä on sivu " + str(page_id)
+
+@app.route("/")
+def index():
+    return "<b>Tervetuloa</b> <i>sovellukseen</i>!"
